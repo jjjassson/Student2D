d2bec308d383320b7eedfb6885d056dede8af141
jjjassson <kuo200493@gmail.com>
2025-06-28 23:55:11 +0800

    20250628
    問題卡在搖桿無法開啟新角色

--- Assets/Scripts/PlayerSetupMenuController.cs
+++ Assets/Scripts/PlayerSetupMenuController.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerSetupMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int playerIndex;
+
+    [SerializeField]
+    private TextMeshProUGUI titleText;
+    [SerializeField]
+    private GameObject readyPanel;
+    [SerializeField]
+    private GameObject menuPanel;
+    [SerializeField]
+    private Button readyButton;
+
+    private float ignoreInputTime = 1.5f;
+    private bool inputEnable;
+
+    public void SetPlayerIndex(int pi)
     {
-        
+        playerIndex = pi;
+        titleText.SetText("Player" + (pi + 1).ToString());
+        ignoreInputTime = Time.time + ignoreInputTime;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time > ignoreInputTime)
+        {
+            inputEnable = true;
+        }
+    }
+
+    public void SetColor(Material color)
+    {
+        if (!inputEnable) { return; }
+        PlayerConfigurationManager.Instance.SetPlayerColor(playerIndex, color);
+        readyPanel.SetActive(true);
+        readyButton.Select();
+        menuPanel.SetActive(false);
+    }
+
+    public void ReadyPlayer()
+    {
+        if (!inputEnable) { return; }
+        PlayerConfigurationManager.Instance.ReadyPlayer(playerIndex);
+        readyButton.gameObject.SetActive(false);
     }
 }
