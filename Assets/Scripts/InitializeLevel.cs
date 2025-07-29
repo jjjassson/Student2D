7533db084ef4eece6d14c4b02d817f4d01d36d72
jjjassson <kuo200493@gmail.com>
2025-07-29 22:11:13 +0800

    prefab
    已更換color

--- Assets/Scripts/InitializeLevel.cs
+++ Assets/Scripts/InitializeLevel.cs
@@ -1,24 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Inezializelevel : MonoBehaviour
+public class InitializeLevel : MonoBehaviour
 {
     [SerializeField]
     private Transform[] playerSpawns;
-    [SerializeField]
-    private GameObject PlayerPrefab;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         var playerConfigs = PlayerConfigurationManager.Instance.GetPlayerConfigs().ToArray();
+
         for (int i = 0; i < playerConfigs.Length; i++)
         {
-            var playerConfig = Instantiate(PlayerPrefab, playerSpawns[i].position, playerSpawns[i].rotation, gameObject.transform);
+            var config = playerConfigs[i];
 
+            // 使用每位玩家選擇的 prefab
+            GameObject playerPrefab = config.SelectedCharacterPrefab;
+
+            if (playerPrefab != null)
+            {
+                // 實例化 prefab
+                var playerObject = Instantiate(playerPrefab, playerSpawns[i].position, playerSpawns[i].rotation);
+
+                // 把對應的 PlayerInput 移到新 prefab 上
+                PlayerInput originalInput = config.Input;
+                originalInput.transform.SetParent(null);
+                originalInput.transform.position = playerSpawns[i].position;
+
+                // 把 PlayerInput 重綁到新的 prefab（如果你 prefab 有控制器需要 Input）
+                originalInput.SwitchCurrentControlScheme(originalInput.currentControlScheme, originalInput.devices.ToArray());
+
+                // 把 Input 移到 prefab 上（可選，看你設計邏輯）
+                originalInput.gameObject.SetActive(false); // 關掉原本的 placeholder
+                originalInput.transform.SetParent(playerObject.transform, true);
+                originalInput.gameObject.SetActive(true);
+            }
+            else
+            {
+                Debug.LogError($"Player {i} 沒有選角色 prefab！");
+            }
         }
     }
-
 }
