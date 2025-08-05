92e59304086e4df8003378a0c186c7a5fbac6709
StonliveShow <poen0412@gmail.com>
2025-08-05 12:14:23 +0800

    Revert "20250805"
    This reverts commit bb4897bfc9cd28f2bbe1d1b5ae740e90b7653ee9.

--- Assets/Scripts/InitializeLevel.cs
+++ Assets/Scripts/InitializeLevel.cs
@@ -16,24 +16,24 @@ public class InitializeLevel : MonoBehaviour
         {
             var config = playerConfigs[i];
 
-
+            
             GameObject playerPrefab = config.SelectedCharacterPrefab;
 
             if (playerPrefab != null)
             {
-
+                
                 var playerObject = Instantiate(playerPrefab, playerSpawns[i].position, playerSpawns[i].rotation);
 
-
+                
                 PlayerInput originalInput = config.Input;
                 originalInput.transform.SetParent(null);
                 originalInput.transform.position = playerSpawns[i].position;
 
-
+                
                 originalInput.SwitchCurrentControlScheme(originalInput.currentControlScheme, originalInput.devices.ToArray());
 
-
-                originalInput.gameObject.SetActive(false);
+                
+                originalInput.gameObject.SetActive(false); 
                 originalInput.transform.SetParent(playerObject.transform, true);
                 originalInput.gameObject.SetActive(true);
             }
