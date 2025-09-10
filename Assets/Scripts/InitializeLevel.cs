559086f63d225b33f69ede93cee0172d66b6ebeb
jjjassson <kuo200493@gmail.com>
2025-09-10 15:52:16 +0800

    控制器
    控制器目前controller可以使用keyboard無法

--- Assets/Scripts/InitializeLevel.cs
+++ Assets/Scripts/InitializeLevel.cs
@@ -2,45 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
 
 public class InitializeLevel : MonoBehaviour
 {
-    [SerializeField]
-    private Transform[] playerSpawns;
+    [SerializeField] private Transform[] playerSpawns; // 玩家生成點
 
-    void Start()
+    private void Start()
     {
         var playerConfigs = PlayerConfigurationManager.Instance.GetPlayerConfigs().ToArray();
 
         for (int i = 0; i < playerConfigs.Length; i++)
         {
             var config = playerConfigs[i];
+            GameObject prefab = config.SelectedCharacterPrefab;
 
-            
-            GameObject playerPrefab = config.SelectedCharacterPrefab;
-
-            if (playerPrefab != null)
-            {
-                
-                var playerObject = Instantiate(playerPrefab, playerSpawns[i].position, playerSpawns[i].rotation);
-
-                
-                PlayerInput originalInput = config.Input;
-                originalInput.transform.SetParent(null);
-                originalInput.transform.position = playerSpawns[i].position;
-
-                
-                originalInput.SwitchCurrentControlScheme(originalInput.currentControlScheme, originalInput.devices.ToArray());
-
-                
-                originalInput.gameObject.SetActive(false); 
-                originalInput.transform.SetParent(playerObject.transform, true);
-                originalInput.gameObject.SetActive(true);
-            }
-            else
+            if (prefab == null)
             {
                 Debug.LogError($"Player {i} 沒有選角色 prefab！");
+                continue;
             }
+
+            // Instantiate 角色 prefab
+            GameObject playerObject = Instantiate(prefab, playerSpawns[i].position, playerSpawns[i].rotation);
+
+            // 取得角色 prefab 上的 PlayerInput
+            PlayerInput playerInput = playerObject.GetComponent<PlayerInput>();
+
+            if (playerInput == null)
+            {
+                Debug.LogError("角色 prefab 必須有 PlayerInput！");
+                continue;
+            }
+
+            // 綁定玩家原本使用的控制器
+            foreach (var device in config.Input.devices)
+            {
+                InputUser.PerformPairingWithDevice(device, playerInput.user);
+            }
+
+            // 設定 playerIndex
+            playerInput.user.AssociateActionsWithUser(playerInput.actions);
         }
     }
 }
